6274e44f29cf941e56072229a32504fc94cf86f8
grv <garv.sharma200212@gmail.com>
2026-07-14 22:12:33 +0530

    added logic to saved the taks ot browser or load it from browser if exist

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('add-btn');
 const taskList = document.getElementById('task-List');
+
+// Try to load saved tasks from localStorage
+const savedTasks = localStorage.getItem('tasks');
+const todos = savedTasks ? JSON.parse(savedTasks) : [];
+
+function addTask() {
+    localStorage.setItem('tasks', JSON.stringify(todos));
+}
