54fab5a9d50882be9a3ff74011779ba6078b41fa
grv <garv.sharma200212@gmail.com>
2026-07-14 21:52:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//select DOM Elements
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('add-btn');
+const taskList = document.getElementById('task-List');
